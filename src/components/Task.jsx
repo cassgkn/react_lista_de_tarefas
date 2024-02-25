7cad1ba56c7a5125d9461c100a39df0213e6b326
Anderson Casimiro <97445047+cassgkn@users.noreply.github.com>
2024-02-25 20:17:24 -0300

    more

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,15 +3,17 @@ const Task = ({ task, onDelete, onToggleDone }) => {
 
     
     return (
-        <li>
-            <span 
-                onClick={onToggleDone}  
-                style={{textDecoration: task.done ? 'line-through' : 'done'}}
-            >
-                {task.text}
-            </span>
+        <>
+            <li>
+                <span 
+                    onClick={onToggleDone}  
+                    style={{textDecoration: task.done ? 'line-through' : 'done'}}
+                >
+                    {task.text}
+                </span>
+            </li>
             <button onClick={onDelete}>Remover</button>
-        </li>
+        </>
     )
 }
 
